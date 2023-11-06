cfc54b406143a1d97a478aa46a73927388316d32
EOussama <oussama_gtacj@live.fr>
2023-11-06 17:21:56 +0100

    Les conditions

--- src/__init__.py
+++ src/__init__.py
@@ -5,9 +5,11 @@ tp1 = __import__('tp1').main
 liste_langages = __import__('liste_langages').main
 tuple_bio = __import__('tuple_bio').main
 dictionnaire = __import__('dictionnaire').main
+conditions = __import__('conditions').main
 
 if __name__ == '__main__':
 		tp1()
 		liste_langages()
 		tuple_bio()
 		dictionnaire()
+		conditions()
