9bac656fc24233f80edba7be904143652f6088a9
EOussama <oussama_gtacj@live.fr>
2023-11-06 16:31:47 +0100

    Les variables

--- src/1-les-variables.py
+++ src/1-les-variables.py
@@ -33,13 +33,57 @@ def question2a():
 def question2b():
 	text, numInt, numDec = "World", 369, 1036.7803
 
-# Écrire un programme qui, à partir de la saisie d’un rayon et d’une hauteur, calcule le volume d’un cône droit
+# 3 - Écrire un programme qui, à partir de la saisie d’un rayon et d’une hauteur, calcule le volume d’un cône droit
 def question3():
-	r = int(input("Enter un rayon: "))
-	h = int(input("Enter une hauteur: "))
+	r = float(input("Enter un rayon: "))
+	h = float(input("Enter une hauteur: "))
 
 	v = 1/3 * math.pi * math.pow(r, 2) * h
 	print(f"Le volume d'un cône qui a une hauteur de {h} et un rayon de {r} est {v}")
+	
+
+# 4 - Une machine découpe dans une plaque, des disques circulaires de rayon rExt, percés d’un trou circulaire de rayon rInt avec rInt < rExt et ne débordant pas du disque Quelle est la surface d’un disque découpé ?
+def question4():
+	# a - trouver les données
+	rExt = float(input("Enter une valeur pour rExt: "))
+	rInt = float(input("Enter une valeur pour rInt: "))
+
+	# b - effectuer les calculs
+	sExt = math.pi * math.pow(rExt, 2)
+	sInt = math.pi * math.pow(rInt, 2)
+	surface = sExt - sInt
+
+	# c - afficher le résultat
+	print(f"La curface de cercle est: {surface}")
+
+# 5 - Écrire un programme qui affiche le type du résultat des instructions suivantes: a = 3 / a = =3
+def question5():
+	a = 3
+	print("Type de a=3 est:", type(a))
+
+	a = '=3'
+	print("Type de a=3 est:", type(a))
+
+# 6 - Écrire un programme, qui ajoute une chaîne de caractères à un nombre entier (Exemple la chaîne ”le chat” et le nombre 3 pour donner le chat 3), et puis renvoyer la taille de chaine
+def question6():
+	a = "le chat"
+	b = 3
+	c = a + " " + str(3)
+
+	print(f"La taille de '{c}' est: {len(c)}")
+
+# 7 - Écrire un programme qui réalise la saisie d’un nombre entier puis affiche la valeur ainsi saisie et son type. Essayer de dépasser la taille maximale des entiers. Expliquer.
+def question7():
+	num = int(input("Entrer un nombre: "))
+
+	print(f"Num = {num}, type = {type(num)}")
+
+# 8 -  Ecrire un programme qui transforme un nombre de base décimale vers la base binaire, et puis renvoyer son adresse mémoire.
+def question8():
+	base10 = float(input('Entere un décimale: '))
+	base2 = bin(int(base10))
+
+	print(f"la valeur {base10} est {base2} en binaire")
 	
 def main():
 	print("1 - Créer un nouveau projet sur PyCharm, et saisir le script suivant et l’enregistrer sous le nom tp1.py")
@@ -54,6 +98,26 @@ def main():
 	question2b()
 	print("------------------------------------------------------\n\n")
 	
-	print("Écrire un programme qui, à partir de la saisie d’un rayon et d’une hauteur, calcule le volume d’un cône droit")
+	print("3 - Écrire un programme qui, à partir de la saisie d’un rayon et d’une hauteur, calcule le volume d’un cône droit")
 	question3()
+	print("------------------------------------------------------\n\n")
+	
+	print("4 - Une machine découpe dans une plaque, des disques circulaires de rayon rExt, percés d’un trou circulaire de rayon rInt avec rInt < rExt et ne débordant pas du disque Quelle est la surface d’un disque découpé ?")
+	question4()
+	print("------------------------------------------------------\n\n")
+	
+	print("5 - Écrire un programme qui affiche le type du résultat des instructions suivantes: a = 3 / a = =3")
+	question5()
+	print("------------------------------------------------------\n\n")
+	
+	print("6 - Écrire un programme, qui ajoute une chaîne de caractères à un nombre entier (Exemple la chaîne ”le chat” et le nombre 3 pour donner le chat 3), et puis renvoyer la taille de chaine")
+	question6()
+	print("------------------------------------------------------\n\n")
+	
+	print("7 - Écrire un programme qui réalise la saisie d’un nombre entier puis affiche la valeur ainsi saisie et son type. Essayer de dépasser la taille maximale des entiers. Expliquer.")
+	question7()
+	print("------------------------------------------------------\n\n")
+	
+	print("8 - Ecrire un programme qui transforme un nombre de base décimale vers la base binaire, et puis renvoyer son adresse mémoire.")
+	question8()
 	print("------------------------------------------------------\n\n")
